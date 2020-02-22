abaed8751e5db94e67e7e14073845b983217e573
JohnCoene <jcoenep@gmail.com>
2020-02-22 20:47:52 +0100

    update dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,6 +4,7 @@ regions <- get_regions()
 
 papers <- tibble::tibble()
 for(r in 1:nrow(regions)){
+  cat("country", r, "\n")
   reg <- get_papers(regions, r)
   papers <- dplyr::bind_rows(papers, reg)
 }
